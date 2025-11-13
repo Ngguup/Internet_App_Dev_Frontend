25a0fa71480036ac89f2fbc60f9d42c1042ff0eb
Konstantin <belikovkon@gmail.com>
2025-11-13 20:09:18 +0300

    lab6 5

--- my-app/src/components/BasicExample/BasicExample.tsx
+++ my-app/src/components/BasicExample/BasicExample.tsx
@@ -6,8 +6,8 @@ function BasicExample() {
   return (
     <header>
       <Navbar expand="lg" className='header' fixed='top'>
-            <Navbar.Brand href="/" className='center-btn'>Прогноз роста объема данных</Navbar.Brand>
-            <Nav.Link href="/factors" className='nav-link-hidden'>Фаторы роста</Nav.Link>
+            <Navbar.Brand href="/Internet_App_Dev_Frontend/" className='center-btn'>Прогноз роста объема данных</Navbar.Brand>
+            <Nav.Link href="/Internet_App_Dev_Frontend/factors" className='nav-link-hidden'>Фаторы роста</Nav.Link>
       </Navbar>
     </header>
   );
