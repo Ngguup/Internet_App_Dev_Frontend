256a0aaaf132550c80279ac56f27647dc9fae13c
Konstantin <belikovkon@gmail.com>
2025-12-03 17:32:47 +0300

    lab6 local1

--- my-app/target_config.ts
+++ my-app/target_config.ts
@@ -1,8 +1,9 @@
 const release_mode = false
 
 // export const api_proxy_addr = "http://192.168.43.57:8080"
-export const api_proxy_addr = "http://192.168.1.10:8080"
+// export const api_proxy_addr = "http://192.168.1.10:8080"
 // export const img_proxy_addr = "http://192.168.0.104:9000"
+export const api_proxy_addr = "http://127.0.0.1:8080"
 export const dest_api = (release_mode) ? api_proxy_addr : "/api"
 // export const dest_img =  (target_tauri) ?  img_proxy_addr : "img-proxy"
 export const dest_root = (release_mode) ? "" : "/Internet_App_Dev_Frontend/"
