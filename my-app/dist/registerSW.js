aac09a5c03950e4772ce98ecf9c9b30f0777d3e3
Konstantin <belikovkon@gmail.com>
2025-12-11 23:42:14 +0300

    lab7 test

---
+++ my-app/dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Internet_App_Dev_Frontend/sw.js', { scope: '/Internet_App_Dev_Frontend/' })})}
